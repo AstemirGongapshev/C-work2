8c09da5d92f099dd132f5ef8b1ce720a16d2beb3
AstemirGongapshev <145905089+AstemirGongapshev@users.noreply.github.com>
2023-12-10 14:09:26 +0300

    change

---
+++ main.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include "fibonacci.h"
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int n;
+	std::cout << "Введите значение n: ";
+	std::cin >> n;
+
+	if (n < 0) {
+		std::cout << "n должно быть неотрицательным числом." << std::endl;
+		return 1;
+	}
+
+	int result = fibonacci(n);
+	std::cout << "Число Фибоначчи для n = " << n << " равно " << result << std::endl;
+	system("pause");
+	return 0;
+}
+
